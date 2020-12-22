9e398f9243d216bf94475ebd0fae8d94f05722db
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-22 13:22:34 -0500

    Fixed comments/documentation

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
  * heap_insert - function to insert max value into heap
- * @root - a double pointer to the root node of the Heap
- * @value - value store in the node to be inserted
+ * @root: a double pointer to the root node of the Heap
+ * @value: value store in the node to be inserted
  * Return: a pointer to the inserted node, or NULL on failure
  */
 heap_t *heap_insert(heap_t **root, int value)
