9c8aa0ac9ef2ca3d22492188548a3ab37ddec703
VictorXRivera <victorxrivera.professional@gmail.com>
2021-04-08 22:17:43 -0400

    first commit

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const request = require('request');
+
+function send (person) {
+  return new Promise(function (resolve, reject) {
+    request(person, function (error, response, body) {
+      if (error || response.code >= 400) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body).name);
+      }
+    });
+  });
+}
+
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  async function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      const people = JSON.parse(body).characters;
+      for (let names = 0; names < people.length; ++names) {
+          const name = await send(people[names]);
+          console.log(name);
+      }
+    }
+  });
