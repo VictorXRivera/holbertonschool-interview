c6c7555b2db9e868c86f6210fcdab9573219a338
VictorXRivera <victorxrivera.professional@gmail.com>
2022-08-15 23:38:55 -0400

    third commit

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,14 +1,22 @@
 #!/usr/bin/node
-const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], async (err, response, body) => {
-    if (err) console.log(err)
-    for (const c of JSON.parse(body).characters) {
-        await new Promise((resolve, reject) => {
-            request(c, (err, response, body) => {
-                console.log(JSON.parse(body).name);
-                resolve();
-            });
-        });
-    }
+const request = require('request');
+const filmId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+
+request(url, async (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  for (const characterId of JSON.parse(body).characters) {
+    await new Promise((resolve, reject) => {
+      request(characterId, (err, response, body) => {
+        if (err) {
+          reject(err);
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
 });
