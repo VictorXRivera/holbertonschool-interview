944efe2a1a7f0b0908aa90af5011126971bb600b
VictorXRivera <victorxrivera.professional@gmail.com>
2022-08-18 22:46:15 -0400

    rain method

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" 
+Given a list of non-negative integers representing walls of width 1,
+calculate how much water will be retained after it rains.
+"""
+
+
+def rain(walls):
+    """
+    Calculates how many units of water is caught
+    by walls 1 unit wide on a 2-d plane
+    """
+    water_amt = 0
+
+    # Traverse rain list
+    for i in range(1, len(walls) - 1):
+
+        # Find max element on left
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+
+        # Find max element on right
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+
+        water_amt += (min(left, right) - walls[i])
+
+    return water_amt
