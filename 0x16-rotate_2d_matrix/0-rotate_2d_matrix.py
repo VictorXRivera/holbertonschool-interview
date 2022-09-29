b5414571026b429675be3c17b081628ecd46454b
VictorXRivera <victorxrivera.professional@gmail.com>
2022-09-28 23:12:37 -0400

    2d Matrix

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,18 @@
+N = 3
+
+def rotate_2d_matrix(matrix):
+    for x in range(0, int(N / 2)):
+        for y in range(x, N-x-1):
+            temp = matrix[x][y]
+            matrix[x][y] = matrix[y][N-1-x]
+            temp = matrix[x][y]
+            matrix[x][y] = matrix[y][N-1-x]
+            matrix[y][N-1-x] = matrix[N-1-x][N-1-y]
+            matrix[N-1-x][N-1-y] = matrix[N-1-y][x]
+            matrix[N-1-y][x] = temp
+
+def displayMatrix(matrix):
+    for i in range(0, N):
+        for j in range(0, N):
+            print(mat[i][j], end=' ')
+        print("")
