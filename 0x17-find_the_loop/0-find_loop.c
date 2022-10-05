6b74456d598349a016f8a0ec95e6a0629fa829d3
VictorXRivera <victorxrivera.professional@gmail.com>
2022-10-05 12:13:54 -0400

    find loop method

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - method that finds loop
+ * inside linked list
+ * @head: head of the list
+ * Return: The address of the node where the loop starts
+ * or NULL if there is no loop
+ **/
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *ptr1 = head;
+	listint_t *ptr2 = head;
+
+	if (!head || !head->next)
+		return (NULL);
+
+	while (ptr2 != NULL && ptr2->next != NULL)
+	{
+		ptr1 = ptr1->next;
+		ptr2 = ptr2->next->next;
+		if (ptr1 == ptr2)
+		{
+			return (ptr2);
+		}
+	}
+
+	return (NULL);
+}
