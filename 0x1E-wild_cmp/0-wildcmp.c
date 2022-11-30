e148584496758ed9c7a223bcf58d292b8eb8df8d
VictorXRivera <victorxrivera.professional@gmail.com>
2022-11-30 05:20:03 -0500

    Wild compare

---
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+  * wildcmp - function to compare two strings
+  * @s1: pointer
+  * @s2: pointer
+  * Return: Success
+  */
+int wildcmp(char *s1, char *s2)
+{
+	if (!*s1)
+	{
+		if (*s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+		return (!*s2);
+	}
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
+}
